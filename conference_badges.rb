bf4c55b648228b0c9f10c7766428a4c556812552
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 16:37:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,3 +22,12 @@ def assign_rooms(array)
   return nuarray
 end
 
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end
